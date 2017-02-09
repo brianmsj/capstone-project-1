9e1dbbc52e3fbd37f2263f430cc51f27e8ad8d2e
marcy <marcy.montross@gmail.com>
2017-02-09 15:47:37 -0600

    get jason from submit, create callback, put data into state.

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-
-
 var state = {
-NASA_BASE_URL: 'https://api.nasa.gov/planetary/earth/imagery',
+    NASA_BASE_URL: 'https://api.nasa.gov/planetary/apod',
+    text: "",
+    image: "" ,
+    date: "",
+}
+
+var addPOD = function(state,url,text,date) {
+    state.url = url;
+    state.text = text;
+    state.date = date;
+}
+
+function getDataFromApi(myDate) {
+
+    var query = {
+        date: myDate,
+        hd: false,
+        api_key: 'eZW6vATkVUO5a2rbju1e9NZyCF88YB7oBr86Sfuc',
+
+    }
+    $.getJSON(state.NASA_BASE_URL, query, callback);
 
 }
 
-function getDataFromApi() {
 
-  var query = {
-    lat: 1.5,
-    lon: 100.75,
-    date: '2014-02-01',
-    api_key: 'eZW6vATkVUO5a2rbju1e9NZyCF88YB7oBr86Sfuc',
-    cloud_score: true
-
-  }
-  var result =$.getJSON(state.NASA_BASE_URL,query);
-  console.log(result);
+function callback(data){
+  var url = data.url;
+  var text = data.explanation;
+  var date = data.date;
+  addPOD(state,url,text,date);
+  console.log(state);
 }
 
+$(function eventHandlers() {
+    $('.js-search-form').submit(function(event) {
+        event.preventDefault();
+        var myDate = $(event.currentTarget).find('.js-query').val();
+        getDataFromApi(myDate);
+    });
+})
 
-$(function(){
-  getDataFromApi();
-});
+
+
+// https://api.nasa.gov/EPIC/api/natural/date/2015-10-31?api_key=eZW6vATkVUO5a2rbju1e9NZyCF88YB7oBr86Sfuc
+// epic_1b_20151031022436_01
+// https://api.nasa.gov/EPIC/archive/enhanced/2016/12/04/png/epic_1b_20151031022436_01.png?api_key=DEMO_KEY
